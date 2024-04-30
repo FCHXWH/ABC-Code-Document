be922d1f7291b5cc1353f293b03ab0b9ac710b66
FCHXWH <37615445+FCHXWH@users.noreply.github.com>
2024-04-30 16:53:33 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'ABC-Code-Document'
+copyright = '2024, Weihua Xiao'
+author = 'Weihua Xiao'
 
 release = '0.1'
 version = '0.1.0'
